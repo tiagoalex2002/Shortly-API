4c0414dd4ddd43af53a472d273aa710ec8183a51
Tiago <tiagoalex0202@gmail.com>
2023-05-23 01:24:36 -0300

    adding route

--- src/Routes/urls.routes.js
+++ src/Routes/urls.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteUrl, getUrlbyId, shortenUrls } from "../Controllers/urls.controllers.js";
+import { deleteUrl, getUrlbyId, shortenUrls, redirectUrl } from "../Controllers/urls.controllers.js";
 import validateSchema from "../Middleware/validateSchema.js";
 import { authValidation } from "../Middleware/authValidation.js";
 import urlSchema from "../Schemas/urlSchema.js";
@@ -8,6 +8,7 @@ const urlRouter= Router();
 
 urlRouter.post("/urls/shorten", authValidation, validateSchema(urlSchema), shortenUrls)
 urlRouter.get("/urls/:id",getUrlbyId)
+urlRouter.get("/urls/open/:shortUrl", redirectUrl)
 urlRouter.delete("/urls/:id", authValidation, deleteUrl)
 
 export default urlRouter;
